74deb5cda41fa4523c333876c62e8e6c6dfe29dd
shuto kikai <yysskikai04@icloud.com>
2023-07-12 15:37:31 +0900

    Gemの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'twiits/index'
+  # get 'twiits/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/twiits", to: "twiits#index"
 end
